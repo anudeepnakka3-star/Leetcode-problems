fd83f36cfffc8ddfe5eefe021c2fbb4fcb0008c8
Nakka Anudeep <anudeepnetha3@gmail.com>
2026-08-07 21:38:17 +0530

    Time: 0 ms (100%), Space: 19.3 MB (25.71%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        idx=-1
+        n=len(nums)
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                idx=i
+                break
+        if idx==-1:
+            nums.reverse()
+            return
+        else:
+            for i in range(n-1,-1,-1):
+                if nums[i]>nums[idx]:
+                    nums[i],nums[idx]=nums[idx],nums[i]
+                    break
+
+
+            nums[idx+1:]=reversed(nums[idx+1:])
+
+        
