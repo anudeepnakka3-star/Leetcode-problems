87cfe73946b17ad58474e201efdcaef3f660462f
Nakka Anudeep <anudeepnetha3@gmail.com>
2026-08-13 22:41:35 +0530

    Time: 310 ms (12.96%), Space: 23.8 MB (66.93%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -19,7 +19,7 @@ class Solution:
                 
                 l=mid+1
             else:
-                ans=mid
+                
                 r=mid-1
-        return ans
+        return l
         
