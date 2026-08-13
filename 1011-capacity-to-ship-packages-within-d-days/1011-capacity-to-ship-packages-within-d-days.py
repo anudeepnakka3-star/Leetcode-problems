73500e929c507604ff35b1d6015cfdceb80af141
Nakka Anudeep <anudeepnetha3@gmail.com>
2026-08-13 22:37:40 +0530

    Time: 287 ms (17.15%), Space: 23.9 MB (31.56%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,25 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def capacity(weights,mid):
+            day=1
+            load=0
+            for i in range(len(weights)):
+                if load+weights[i]>mid:
+                    day+=1
+                    load=weights[i]
+                else:
+                    load+=weights[i]
+            return day
+        
+        l=max(weights)
+        r=sum(weights)
+        while l<=r:
+            mid=(l+r)//2
+            if capacity(weights,mid)>days:
+                
+                l=mid+1
+            else:
+                ans=mid
+                r=mid-1
+        return ans
+        
