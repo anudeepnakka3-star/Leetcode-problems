b04260c213355414087db41c4520abdddb086658
Nakka Anudeep <anudeepnetha3@gmail.com>
2026-08-16 16:38:55 +0530

    Time: 35 ms (5.84%), Space: 19.8 MB (95.64%) - LeetHub

---
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+        rows=len(mat)
+        cols=len(mat[0])
+        c=0
+        index=0
+        for i in range(rows):
+            cnt=0
+            for j in range(cols):
+                if mat[i][j]==1:
+                    cnt+=1
+                if cnt>c:
+                    c=cnt
+                    index=i
+        return [index,c]
+        
