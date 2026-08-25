3da0e9acc02fe67346bd1fa457d9f6b2d401ed2a
Nakka Anudeep <anudeepnetha3@gmail.com>
2026-08-25 14:53:05 +0530

    Time: 0 ms (100%), Space: 19 MB (99.02%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,21 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        ans=x^y
+        
+        def convert_binary(num):
+            result=""
+            while num>0:
+                if num%2==1:
+                    result+="1"
+                else:
+                    result+"0"
+                num=num//2
+            return result[::-1]
+        res=convert_binary(ans)
+        c=0
+        for i in res:
+            if i=="1":
+                c+=1
+        return c
+
+        
